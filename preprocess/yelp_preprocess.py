ed4f2c35851d93acd87940653ef3a0ec648a237e
Varun Venkatesh <varunven@usc.edu>
2022-04-06 16:54:50 -0700

    Fix datetime bug

--- preprocess/yelp_preprocess.py
+++ preprocess/yelp_preprocess.py
@@ -8,7 +8,7 @@ import json
 import pprint
 import argparse
 import os
-import datetime
+from datetime import datetime
 import logging
 import pandas as pd
 
@@ -43,7 +43,8 @@ def convert_data(input_file, output_file):
                 logging.info(count)
         for b in business_dict:
             business_dict[b].sort()
-            fout.write("%s\n" % json.dumps({"business": b, "reviews": business_dict[b]}))
+            fout.write("%s\n" % json.dumps(
+                {"business": b, "reviews": business_dict[b]}))
 
 
 if __name__ == "__main__":
@@ -59,74 +60,86 @@ if __name__ == "__main__":
                         type=str,
                         help="")
     parser.add_argument("--data_split",
-                        action='store_true', 
+                        action='store_true',
                         default=True,
                         help="")
     parser.add_argument("--num_review",
                         type=int,
                         default=50,
-                        help="Number of reviews for computing average rating")    
-    
-    args = parser.parse_args() 
-    logging.info(f"args: {args}")   
+                        help="Number of reviews for computing average rating")
+
+    args = parser.parse_args()
+    logging.info(f"args: {args}")
 
     restaurant_business_ids = set()
-    with open(os.path.join(args.yelp_data_dir, "business.json"), "r") as f:    
-        for line in f:  
-            if line:    
+    with open(os.path.join(args.yelp_data_dir, "business.json"), "r") as f:
+        for line in f:
+            if line:
                 json_content = json.loads(line)
-                if json_content["categories"] != None:                        
-                    categories = [val.lower().strip() for val in json_content["categories"].split(",")]
+                if json_content["categories"] != None:
+                    categories = [val.lower().strip()
+                                  for val in json_content["categories"].split(",")]
                     if "restaurants" in categories:
-                        restaurant_business_ids.add(json_content["business_id"])
-    
-    logging.info(f"Finished reading the business.json file. There are {len(restaurant_business_ids)} unique restaurants in the dataset.")
+                        restaurant_business_ids.add(
+                            json_content["business_id"])
 
-    grouped_reviews_filepath = os.path.join(args.yelp_data_dir, "grouped_reviews.jsonlist.gz")
+    logging.info(
+        f"Finished reading the business.json file. There are {len(restaurant_business_ids)} unique restaurants in the dataset.")
+
+    grouped_reviews_filepath = os.path.join(
+        args.yelp_data_dir, "grouped_reviews.jsonlist.gz")
     if not os.path.exists(grouped_reviews_filepath):
-        logging.info(f'Grouped reviews not exist. Building grouped reviews at {grouped_reviews_filepath}')
-        convert_data(os.path.join(args.yelp_data_dir, "review.json"), grouped_reviews_filepath)
+        logging.info(
+            f'Grouped reviews not exist. Building grouped reviews at {grouped_reviews_filepath}')
+        convert_data(os.path.join(args.yelp_data_dir,
+                     "review.json"), grouped_reviews_filepath)
         logging.info(f'Grouped reviews file built')
 
-    logging.info(f"converting grouped reviews into resutaurant only reviews and compute average of first {args.num_review} reviews")
-    out_file = os.path.join(args.output_dir, f"yelp_10reviews_{args.num_review}avg.jsonl.gz")
+    logging.info(
+        f"converting grouped reviews into resutaurant only reviews and compute average of first {args.num_review} reviews")
+    out_file = os.path.join(
+        args.output_dir, f"yelp_10reviews_{args.num_review}avg.jsonl.gz")
     with gzip.open(grouped_reviews_filepath, 'rt') as f_in,  gzip.open(out_file, "wt") as fout:
         for l in f_in:
             r = json.loads(l)
             if r['business'] not in restaurant_business_ids:
                 continue
-            if len(r['reviews']) >=args.num_review:
-                tmp = {"reviews":[], "scores":[]}
+            if len(r['reviews']) >= args.num_review:
+                tmp = {"reviews": [], "scores": []}
                 tmp['business'] = r['business']
                 for i in range(10):
                     tmp["reviews"].append(r["reviews"][i][2])
                     tmp["scores"].append(r["reviews"][i][3])
-                tmp["avg_score"] = sum([r["reviews"][j][3] for j in range(args.num_review)])/args.num_review
+                tmp["avg_score"] = sum(
+                    [r["reviews"][j][3] for j in range(args.num_review)])/args.num_review
                 fout.write("%s\n" % json.dumps(tmp))
 
     if args.data_split:
 
-        split_datapath = os.path.join(args.output_dir, f"{args.num_review}reviews")
+        split_datapath = os.path.join(
+            args.output_dir, f"{args.num_review}reviews")
         if not os.path.exists(split_datapath):
-            logging.info(f'Split datapath not exist. Create split data dir at {split_datapath}')
+            logging.info(
+                f'Split datapath not exist. Create split data dir at {split_datapath}')
             os.makedirs(split_datapath, exist_ok=True)
-        
+
         def dump_jsonl_gz(obj, outpath):
             # obj is list of json
             with gzip.open(outpath, "wt") as fout:
                 for o in obj:
                     fout.write("%s\n" % json.dumps(o))
-        
+
         splits = ["train", "dev", "test"]
         split_ids = {}
         for s in splits:
-            split_ids[s] = set(pd.read_csv(f"preprocess/{s}_business_ids.csv").business.values)
-        
+            split_ids[s] = set(pd.read_csv(
+                f"preprocess/{s}_business_ids.csv").business.values)
+
         reviews = []
         with gzip.open(out_file, 'rt') as f:
             for line in f:
                 reviews.append(json.loads(line))
-            
+
         for split, ids in split_ids.items():
             split_reviews = []
             for review in reviews:
@@ -135,6 +148,7 @@ if __name__ == "__main__":
 
             storepath = os.path.join(split_datapath, f"{split}.jsonl.gz")
             dump_jsonl_gz(split_reviews, storepath)
-            logging.info(f"Data split length: {split} ({len(split_reviews)}), stored at: {storepath}")
+            logging.info(
+                f"Data split length: {split} ({len(split_reviews)}), stored at: {storepath}")
 
     logging.info("Finished!")
